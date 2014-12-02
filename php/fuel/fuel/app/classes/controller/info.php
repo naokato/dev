bee516c238d774ca65b5a1e19f37172e3d0a38c2
naokato <naokato@www30382ue.sakura.ne.jp>
2014-12-03 00:28:17 +0900

    add Controller_Rest example

--- php/fuel/fuel/app/classes/controller/info.php
+++ php/fuel/fuel/app/classes/controller/info.php
@@ -2,6 +2,7 @@
 
 class Controller_Info extends Controller_Rest
 {
+    # http://***/public/info/prof.json?name=sato&age=20
     public function get_prof()
     {
         $name = Input::get('name');
@@ -9,6 +10,15 @@ class Controller_Info extends Controller_Rest
         $this->response(array(
             'name' => $name,
             'age'  => $age,
+        ));
+    }
+    
+    # http://***/public/info/city/tokyo/akasaka.json
+    public function get_city($pref, $addr)
+    {
+        $this->response(array(
+            'pref'  => $pref,
+            'addr'  => $addr,
         ));
     }
 }
